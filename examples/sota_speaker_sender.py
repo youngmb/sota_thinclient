24c4e42af7fccce15b632a44af0ff7ba363cd14d
youngmb <young@cs.umanitoba.ca>
2026-06-05 17:27:29 -0500

    polished the stream resampler and added sample files

--- examples/sota_speaker_sender.py
+++ examples/sota_speaker_sender.py
@@ -2,70 +2,53 @@ import time
 import wave
 import numpy as np
 
-from scipy.signal import resample #for audio resampling -- install for example only
-
 from sota_thinclient import ConnectionManager
+from sota_thinclient.http_audio_stream import StreamingMonoResampler
 
-SOTA_IP = "192.168.0.23"
-# SOTA_IP = "10.151.63.71"
+# SOTA_IP = "192.168.0.23"
+SOTA_IP = "10.151.63.71"
 HTTP_PORT = "8080"
 UDP_PORT = 52002
-WAV_FILE = "sample.wav"
-
-def get_resampled_audio_data(wav_file, target_channels, target_samplerate, target_sample_size_bits):   # Loads wav and converts to sota-friendly format
-    with wave.open(wav_file, 'rb') as wf:
-        print(
-            f"Loaded file: {wf.getnchannels()} channels, {wf.getsampwidth() * 8}-bit, {wf.getframerate()} Hz, {wf.getnframes()} frames")
-
-        # Convert bytes to numpy array
-        dtype_map = {1: np.int8, 2: np.int16, 4: np.int32}  # BYTES to np type. Need updating for floating types
-        audio = np.frombuffer(wf.readframes(wf.getnframes()),  # wf gives bytes, so we read as the intended type
-                              dtype=dtype_map[wf.getsampwidth()])
-        return audio.tobytes()
-    #     # If stereo+, reshape (frames, channels)
-    #     if wf.getnchannels() > 1:
-    #         audio = audio.reshape(-1, wf.getnchannels())  # makes len x channels matrix
-    #         audio = audio[:, :target_channels]  # discard unused channels.
-    #
-    #     # resample to Sota's samplerate and datatype
-    #     out_type = np.dtype(np.dtype(f'i{target_sample_size_bits // 8}'))
-    #     num_target_frames = int(len(audio) * target_samplerate / wf.getframerate())
-    #     resampled_audio = resample(audio, num_target_frames, axis=0)  # results in floating point,
-    #     # resampled_audio = np.clip(resampled_audio, np.iinfo(out_type).min, np.iinfo(out_type).max)
-    #     resampled_audio = resampled_audio.astype(out_type)
-    #
-    # return resampled_audio.tobytes()
+WAV_FILE = "sample-24.wav"
 
 # our central sota connection manager
 sota = ConnectionManager(SOTA_IP, HTTP_PORT)
 sota.speaker.enable(data_udp_port=UDP_PORT, restart_if_enabled=True) # tries to get the server to start listening for our speaker UDP stream
 speaker_state = sota.speaker.get_state()
 
-data = get_resampled_audio_data(WAV_FILE,
-                      speaker_state['channels'],
-                      speaker_state['sampleRate'],
-                      speaker_state['sampleSize_bits'])
+with wave.open(WAV_FILE, 'rb') as wf:
+    print(f"Loaded file: {wf.getnchannels()} channels, {wf.getsampwidth() * 8}-bit, {wf.getframerate()} Hz, {wf.getnframes()} frames")
+    wav_data = wf.readframes(wf.getnframes())
+
+    resampler = StreamingMonoResampler(
+        source_rate=wf.getframerate(),
+        source_dtype= np.dtype(f'int{wf.getsampwidth() * 8}'),
+        target_rate= speaker_state['sampleRate'],
+        target_dtype= np.dtype(f'int'+str(speaker_state['sampleSize_bits']))
+    )
+
+    chunk_size = speaker_state['bufferSize']  # simulate a reasonable streaming slice for our resampling
+    resampled_data = b""
+    for i in range(0, len(wav_data), chunk_size):  # could do in one go but simulate streaming for testing
+        resampled_data += resampler.resample_chunk(wav_data[i:i+chunk_size])
 
 ##crudely simulate streaming audio
 buffer_size = speaker_state['bufferSize']
 frame_size = (speaker_state['channels'] * (speaker_state['sampleSize_bits']//8))
 buffer_s = (buffer_size / frame_size) / speaker_state['sampleRate']
 print(f"Buffer of {speaker_state['bufferSize']} is {buffer_s:.3f} s")
-
 print(sota.speaker.get_state())
 
-sota_buffer_len = 112
-
-initial_buffer = 20  # send packets without delay to get the buffer warmed up
+initial_buffer = 10  # send packets without delay to get the buffer warmed up
 next_time = time.perf_counter()
-for i in range(0, len(data), buffer_size):
-    chunk = data[i:i+buffer_size]
+for i in range(0, len(resampled_data), buffer_size):
+    chunk = resampled_data[i:i+buffer_size]
 
     remainder = len(chunk) % buffer_size
     if remainder != 0:
         chunk = chunk + bytes(buffer_size - remainder)
     sota.speaker.data_queue.put(chunk, block=False)
-    print(len(chunk))
+    # print(len(chunk))
 
     next_time += buffer_s
     remaining = next_time - time.perf_counter()
--- sota_thinclient/http_audio_stream.py
+++ sota_thinclient/http_audio_stream.py
@@ -1,8 +1,8 @@
 import queue
-from operator import truediv
+import numpy as np
+from scipy import signal
 
 from sota_thinclient.http import HTTPConnector
-from sota_thinclient.udp_stream import UDPStreamReceiver
 
 _FIELD_ENABLED = "enabled"
 _FIELD_VOLUME = "volume"
@@ -50,3 +50,40 @@ class HTTPAudioStream(HTTPConnector):
 
         payload[_FIELD_ENABLED] = False
         return self._post_state(payload)
+
+class StreamingMonoResampler:
+    def __init__(self,
+                 source_rate: int,
+                 target_rate: int,
+                 source_dtype : np.dtype = np.dtype('int16'),
+                 target_dtype: np.dtype = np.dtype('int16'),
+                 ):
+        self._source_sr = source_rate
+        self._target_sr = target_rate
+        self._source_dtype = source_dtype
+        self._target_dtype = target_dtype
+
+        self._sample_debt = 0.0  # fractional samples owed to next chunk
+        self._last_sample = None   # continuity across samples
+
+    def resample_chunk(self, raw_audio_bytes: bytes) -> bytes:
+
+        if self._source_sr == self._target_sr:
+            return raw_audio_bytes
+
+        new_chunk = np.frombuffer(raw_audio_bytes, dtype=self._source_dtype)
+
+        if self._last_sample is not None:
+            new_chunk = np.concatenate(([self._last_sample], new_chunk))
+        self._last_sample = new_chunk[-1]  # save last sample for nex ttime. note np works in frames
+
+        exact_output = len(new_chunk) * self._target_sr / self._source_sr + self._sample_debt
+        n_output = int(exact_output)
+        self._sample_debt = exact_output - n_output  # carry over fraction to next chunk
+
+        x_old = np.linspace(0, 1, len(new_chunk))
+        x_new = np.linspace(0, 1, n_output)
+        resampled = np.interp(x_new, x_old, new_chunk.astype(np.float32))[1:]  # trim the prepended interpolated entry
+
+        return np.clip(resampled, np.iinfo(self._target_dtype).min,
+                       np.iinfo(self._target_dtype).max).astype(self._target_dtype).tobytes()
